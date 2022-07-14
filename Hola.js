893e9c1f008b99e99858165abf7b9d105ce0d7af
EdwinRodriguezValle <rodriguez.valle.edwin@gmail.com>
2022-07-14 16:39:37 +0200

    numero

--- Hola.js
+++ Hola.js
@@ -1,3 +1,13 @@
 while (5< 10){
     console.log("I am a god!!")
 }
+
+
+const numero = [1, 2,35,5,5,5,];
+for (let i = 0; i < length.numero; i++) {
+    if(numero(i) < 20){
+        console.log("baila como un gato")
+    }else{
+        console.log("Voz es un estupido sin cerebro")
+    }
+    }
